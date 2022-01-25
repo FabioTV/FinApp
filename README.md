6148e44beebb3cb0d722ef8a6f159b3c779208dc
FabioTV <fabiothomaz.97@gmail.com>
2022-01-25 17:52:37 -0300

    [feature] FA005 - Edit & delete acc, get balance

--- README.md
+++ README.md
@@ -6,15 +6,17 @@ Requisitos <br>
 &ensp;[x] Deve ser possível buscar o extrato bancário do cliente <br>
 &ensp;[x] Deve ser possível realizar um depósito <br>
 &ensp;[x] Deve ser possível realizar um saque <br>
-&ensp;[] Deve ser possível buscar o extrato bancário do cliente por data <br>
-&ensp;[] Deve ser possível atualizar dados da conta do cliente <br>
-&ensp;[] Deve ser possível obter dados da conta do cliente <br>
-&ensp;[] Deve ser possível deletar uma conta <br>
+&ensp;[x] Deve ser possível buscar o extrato bancário do cliente por data <br>
+&ensp;[x] Deve ser possível atualizar dados da conta do cliente <br>
+&ensp;[x] Deve ser possível obter dados da conta do cliente <br>
+&ensp;[x] Deve ser possível deletar uma conta <br>
+&ensp;[x] Deve ser possível consultar o saldo da conta <br>
+
 <br>
 Regras de negócio <br>
 &ensp;[x] Não deve ser possível cadastrar uma conta com CPF já existente <br>
 &ensp;[x] Não deve ser possível fazer depósito em uma conta não existente <br>
 &ensp;[x] Não deve ser possível buscar extrato em uma conta não existente <br>
 &ensp;[x] Não deve ser possível fazer saque em uma conta não existente <br>
-&ensp;[] Não deve ser possível excluir uma conta não existente <br>
-&ensp;[] Não deve ser possível fazer saque quando o saldo for insuficiente <br>
+&ensp;[x] Não deve ser possível excluir uma conta não existente <br>
+&ensp;[x] Não deve ser possível fazer saque quando o saldo for insuficiente <br>
--- src/index.js
+++ src/index.js
@@ -81,7 +81,7 @@ app.post("/deposit", verifyIfExistAccountCPF, (request, response) =>{
 
 app.post("/withdraw", verifyIfExistAccountCPF, (request, response)=>{
     const { amount } = request.body;
-    const customer = request.customer;
+    const {customer} = request;
     const balance = getBalance(customer.statement);
 
     if (balance < amount){
@@ -97,6 +97,44 @@ app.post("/withdraw", verifyIfExistAccountCPF, (request, response)=>{
     customer.statement.push(statementOperation);
 
     return response.status(201).send();
+});
+
+app.get("/statement/date", verifyIfExistAccountCPF, (request,response) => {
+    const { date } = request.query;
+    const { customer } = request;
+    const dateFormat = new Date(date + " 00:00");
+    const statement = customer.statement.filter((statement)=> statement.created_at.toDateString() === new Date(dateFormat).toDateString());
+    return response.json(statement);
+});
+
+app.put("/account", verifyIfExistAccountCPF, (request, response) => {
+    const { name } = request.body;
+    const { customer } = request;
+
+    customer.name = name;
+
+    return response.status(201).send();
+});
+
+app.get("/account", verifyIfExistAccountCPF, (request,response) => {
+    const { customer } = request;
+    
+    return response.json(customer);
+});
+
+app.delete("/account", verifyIfExistAccountCPF, (request, response)=> {
+    const { customer } = request.customer;
+
+    customers.splice(customer, 1);
+
+    return response.status(200).json(customers);
+});
+
+app.get("/balance", verifyIfExistAccountCPF, (request, response) => {
+    const { customer } = request;
+    const balance = getBalance(customer.statement);
+
+    return response.json(balance);
 });
 
 app.listen(3333);
